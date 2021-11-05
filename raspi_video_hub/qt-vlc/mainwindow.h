a35b120d36c2bee3950703d8555d549505868a82
Dmitry Ostapenko <dostapazov@gmail.com>
2021-11-05 19:06:36 +0300

    prevent permanently recreate m_player (increase CAMERA_WDT_INTERVAL  to 20sec).

--- raspi_video_hub/qt-vlc/mainwindow.h
+++ raspi_video_hub/qt-vlc/mainwindow.h
@@ -23,7 +23,7 @@
 
 #define UPDATE_EXIT_CODE 77
 
-#define CAMERA_WDT_INTERVAL     250
+#define CAMERA_WDT_INTERVAL     20000
 #define VLOG_WDT_INTERVAL       20*1000
 
 //typedef struct CAM_CFG
@@ -40,88 +40,88 @@ typedef cam_logger_vlc logger_t;
 
 class MainWindow : public QMainWindow, private Ui::MainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = Q_NULLPTR);
-    ~MainWindow();
+	explicit MainWindow(QWidget* parent = Q_NULLPTR);
+	~MainWindow();
 
 
 protected:
-    void closeEvent(QCloseEvent *event);
+	void closeEvent(QCloseEvent* event);
 
 private:
 
-    void start_cam_monitor();
-    void start_time_sync();
-    void init_gpio  ();
-    void init_libvlc();
-    void init_config();
-    void init_uart  ();
-    void deinitUART ();
-    void deinit_all ();
-    void start_loggers();
-    void start_file_deleter();
-    void readCPUtemper();
- QString whoami();
-    bool check_media_drive();
-    void showEvent  (QShowEvent  * event);
-    void start_cam_switch(bool enable);
-    void handle_uart_packet(PCK_Header_t & header, int offset);
-    void deinit_player();
+	void start_cam_monitor();
+	void start_time_sync();
+	void init_gpio  ();
+	void init_libvlc();
+	void init_config();
+	void init_uart  ();
+	void deinitUART ();
+	void deinit_all ();
+	void start_loggers();
+	void start_file_deleter();
+	void readCPUtemper();
+	QString whoami();
+	bool check_media_drive();
+	void showEvent  (QShowEvent*   event);
+	void start_cam_switch(bool enable);
+	void handle_uart_packet(PCK_Header_t& header, int offset);
+	void deinit_player();
 Q_SIGNALS:
-       void cam_switch(quint8 cam_id);
+	void cam_switch(quint8 cam_id);
 
 private:
-       void setSystemDateTime(QDateTime dt);
+	void setSystemDateTime(QDateTime dt);
 
 private slots:
 
-    void onUARTread();
-    void onParse();
+	void onUARTread();
+	void onParse();
 
-    void on_blink();
-    void on_moncam_timeout();
-    void on_monloger_timeout();
-    void on_cam_switch(quint8 camNum);
+	void on_blink();
+	void on_moncam_timeout();
+	void on_monloger_timeout();
+	void on_cam_switch(quint8 camNum);
 
-    void cam_time_synchronized(bool ok);
-    void cam_time_difference  (const QDateTime& dt, const qint64 & diff);
-    void mon_player_events    (const libvlc_event_t event);
+	void cam_time_synchronized(bool ok);
+	void cam_time_difference  (const QDateTime& dt, const qint64& diff);
+	void mon_player_events    (const libvlc_event_t event);
 
 
-    void on_bTestUpdate_clicked();
+	void on_bTestUpdate_clicked();
 
 private:
 
-    vlc::vlc_player     * m_mon_player   = Q_NULLPTR;
-    QString m_vlog_root  = "d:/rasp_media/VLOG6";
-    int     m_vlog_tmlen = 3600;
-    QVector<logger_t *>   loggers;
+	vlc::vlc_player*      m_mon_player   = Q_NULLPTR;
+	QString m_vlog_root  = "d:/rasp_media/VLOG6";
+	int     m_vlog_tmlen = 3600;
+	QVector<logger_t*>   loggers;
 
-    quint8 camIndex = 0;
-    QTimer blinker ;
-    QTimer parser  ;
-    QTimer moncam_timer;
-    QTimer monlog_timer;
-    int                led_state = 1;
-    enum               vlogger_state_e {vl_disable,vl_enable,vl_working};
-    vlogger_state_e    m_vlogger_state = vl_disable;
+	quint8 camIndex = 0;
+	QTimer blinker ;
+	QTimer parser  ;
+	QTimer moncam_timer;
+	QTimer monlog_timer;
+	int                led_state = 1;
+	enum               vlogger_state_e {vl_disable, vl_enable, vl_working};
+	vlogger_state_e    m_vlogger_state = vl_disable;
 
-    QByteArray         rxBuf;
-    QSerialPort      * uart = Q_NULLPTR;
+	QByteArray         rxBuf;
+	QSerialPort*       uart = Q_NULLPTR;
 
-    FileDeleterThread * file_deleter = Q_NULLPTR;
+	FileDeleterThread* file_deleter = Q_NULLPTR;
 
-    CamTimeSync     cam_time_sync;
-    int             cam_time_synchro = -1;
-    PCK_STATE_t appState;
-    static const char* const vlcArgs[];
-    static bool    do_rename_recorder  ();
-    static QString get_update_file_name();
-    static void    check_need_update   ();
+	CamTimeSync     cam_time_sync;
+	int             cam_time_synchro = -1;
+	PCK_STATE_t appState;
+	static const char* const vlcArgs[];
+	static bool    do_rename_recorder  ();
+	static QString get_update_file_name();
+	static void    check_need_update   ();
 
-    bool is_cam_online = false;
+	bool is_cam_online = false;
 
 };
 
