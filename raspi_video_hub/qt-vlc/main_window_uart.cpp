20b5bb644c0bfc823a6cbffae5819367843c6081
Dmitry <dostapazov@gmail.com>
2021-11-18 00:05:29 +0300

    save old version

--- raspi_video_hub/qt-vlc/main_window_uart.cpp
+++ raspi_video_hub/qt-vlc/main_window_uart.cpp
@@ -166,3 +166,100 @@ void MainWindow::setSystemDateTime(QDateTime dt)
     Q_UNUSED(res)
     appLog::write(0, QString("Accepted new date/time: %1").arg(dt.toString()));
 }
+
+
+/*
+void MainWindow::onParse()
+{
+    constexpr int EMPTY_PACKET_SIZE = (sizeof(PCK_Header_t) + sizeof(quint32));
+    quint16 offset = 0;
+    PCK_Header_t header;
+    quint32 crc = 0;
+    bool hasPck = (rxBuf.count() > EMPTY_PACKET_SIZE);
+
+while (hasPck)
+{
+  while ((CU_SIGNATURE_ != rxBuf[offset]) && (offset < rxBuf.count()))
+    offset++;
+
+  rxBuf.remove(0, offset);
+
+  hasPck = false;
+
+  if (rxBuf.count() >= EMPTY_PACKET_SIZE)
+  {
+    memcpy(&header, rxBuf.constData(), sizeof(header));
+    offset = sizeof(header);
+
+    if (rxBuf.count() >= (offset + EMPTY_PACKET_SIZE))
+    {
+      quint32 pcrc = 0;
+      crc = crc32(rxBuf, 0, header.size + offset);
+      memcpy(&pcrc, rxBuf.constData() + offset + header.size, sizeof(pcrc));
+
+      if ((crc == pcrc) || 1)
+      {
+        if (header.devId == appConfig::value("DEV/ID").toUInt())
+        {
+          switch (header.pckType)
+          {
+            case PCT_SHUTDOWN:
+            {
+              close();
+              break;
+            }
+            case PCT_CAM_SWITCH:
+            {
+              quint8 id = 0;
+              if (header.size == sizeof(id))
+              {
+                id = (quint8)rxBuf.at(offset);
+                if (id)
+                  emit cam_switch(id - 1);
+              }
+
+              break;
+            }
+            case PCT_STATE:
+            {
+              if (header.size == 0)
+                uart->write(makePck(PCT_STATE, QByteArray((char*)&appState, sizeof(appState))));
+              break;
+            }
+            case PCT_DATETIME:
+            {
+              if (header.size == sizeof(PCK_DateTime_t))
+              {
+                PCK_DateTime_t* pl = (PCK_DateTime_t*)&rxBuf.data()[offset];
+                QDateTime dt;
+                dt.setDate(QDate(pl->year + 2000, pl->mounth, pl->day));
+                dt.setTime(QTime(pl->hour, pl->min, pl->sec));
+                setSystemDateTime(dt);
+              }
+              break;
+            }
+            default :
+              appLog::write(5, tr("UART unknownd packet type %1").arg(int(header.pckType)));
+              break;
+          }
+        }
+        else
+          appLog::write(5, tr("UART different devId [self %1] packet %2 type %3 size %4").arg(appConfig::value("DEV/ID").toUInt()).arg(int(header.devId)).arg(int(header.pckType)).arg(int(header.devId)).arg(int(header.size)));
+
+        offset += header.size + sizeof(crc);
+      }
+      else
+      {
+        offset = 1;
+        appLog::write(5, "UART error crc");
+      }
+
+      rxBuf.remove(0, offset);
+      hasPck = (rxBuf.count() > EMPTY_PACKET_SIZE);
+    }
+  }
+}
+}
+
+ */
+
