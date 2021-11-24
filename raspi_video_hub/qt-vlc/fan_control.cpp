234dbbccaf185856befffe766e60544c4a608c9d
Dmitry Ostapenko <dostapazov@gmail.com>
2021-11-24 20:57:31 +0300

    fan_control refactoring

--- raspi_video_hub/qt-vlc/fan_control.cpp
+++ raspi_video_hub/qt-vlc/fan_control.cpp
@@ -5,67 +5,68 @@
 void MainWindow::readCPUtemper()
 {
 
-    FILE* thermal;
-    int n = 0;
+	FILE* thermal;
+	int n = 0;
 #ifdef Q_OS_LINUX
-    const char* temper_file_name = "/sys/class/thermal/thermal_zone0/temp";
+	const char* temper_file_name = "/sys/class/thermal/thermal_zone0/temp";
 #else
-    const char* temper_file_name = "d:/temp.txt";
+	const char* temper_file_name = "d:/temp.txt";
 #endif
 
-    int onTemper  = appConfig::value("FAN/StartTemper").toInt() ;
-    int offTemper = appConfig::value("FAN/StopTemper" ).toInt() ;
-    onTemper  = 1000 * qMax(onTemper, 45);
-    offTemper = 1000 * qMax(offTemper, 40);
+	int onTemper  = appConfig::value("FAN/StartTemper").toInt() ;
+	int offTemper = appConfig::value("FAN/StopTemper" ).toInt() ;
+	onTemper  = 1000 * qMax(onTemper, 45);
+	offTemper = 1000 * qMax(offTemper, 40);
 
 
 
-    thermal = fopen(temper_file_name, "r");
+	thermal = fopen(temper_file_name, "r");
 
-    if (thermal)
-    {
-        int rv = 0;
-        n = fscanf(thermal, "%d", &rv);
-        fclose(thermal);
-        appState.temper = static_cast<quint16>(rv);
+	if (thermal)
+	{
+		int rv = 0;
+		n = fscanf(thermal, "%d", &rv);
+		fclose(thermal);
+		appState.temper = static_cast<quint16>(rv);
+	}
 
+	if ( !thermal || !n )
+	{
+		appState.temper = static_cast<quint16>(onTemper + 1);
+		if (appState.fanState != FAN_ON)
+		{
+			appLog::write(LOG_LEVEL_FAN_CTRL, "error get temperature from mon file. switch fan ON");
+			fanSwitch(true);
+			return;
+		}
+	}
 
-    }
+	if ((appState.temper >= onTemper) )
+	{
+		fanSwitch(true);
+		return;
+	}
 
-    if ( !thermal || !n )
-    {
-        appState.temper = static_cast<quint16>(onTemper + 1);
-        if (1 != appState.fanState) // Вентилятор выключен пишем в лог и включаем принудительно
-            appLog::write(LOG_LEVEL_FAN_CTRL, "error get temperature from mon file. switch fan ON");
-    }
+	if ((appState.temper <= offTemper) && (appState.fanState != FAN_OFF))
+	{
+		fanSwitch(false);
+		return;
+	}
 
-    //qDebug("current temp %u  fan state %d onTemp %d offTemp %d ",(unsigned int)appState.temper, (int)appState.fanState,onTemper,offTemper);
-    bool fan_state_changed = false;
-
-    if ((appState.temper >= onTemper) && (appState.fanState != 1))
-    {
-        appState.fanState = 1;
-        fan_state_changed = true;
-        QString str = tr("Temper  %1 > %2, starting fan").arg(appState.temper).arg(onTemper);
-        appLog::write(LOG_LEVEL_FAN_CTRL, str);
-    }
-
-    if ((appState.temper <= offTemper) && (appState.fanState != 0))
-    {
-        appState.fanState = 0;
-        fan_state_changed = true;
-        QString str = tr("Temper  %1 < %2, stopping fan").arg(appState.temper).arg(onTemper);
-        appLog::write(LOG_LEVEL_FAN_CTRL, str);
-    }
-    QString str = QString("Current temper  %1, fan_state %2 changed %3")
-                  .arg(appState.temper)
-                  .arg(appState.fanState)
-                  .arg(fan_state_changed);
-    appLog::write(LOG_LEVEL_FAN_CTRL, str);
-
-    if (fan_state_changed)
-    {
-        digitalWrite(PIN_FAN, appState.fanState);
-    }
 }
+
+bool MainWindow::fanSwitch(bool on)
+{
+	quint8 newState = on ? FAN_ON : FAN_OFF;
+	if (appState.fanState != newState)
+	{
+		QString str = tr("Temper  %1 fan is %2").arg(appState.temper).arg(on ? "started" : "stopped");
+		appLog::write(LOG_LEVEL_FAN_CTRL, str);
+		appState.fanState = newState;
+		digitalWrite(PIN_FAN, 1);
+		return true;
+	}
+	return false;
+}
+
 
--- raspi_video_hub/qt-vlc/mainwindow.h
+++ raspi_video_hub/qt-vlc/mainwindow.h
@@ -30,101 +30,103 @@
 
 class MainWindow : public QMainWindow, private Ui::MainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget* parent = Q_NULLPTR);
-    ~MainWindow();
+	explicit MainWindow(QWidget* parent = Q_NULLPTR);
+	~MainWindow();
 
 Q_SIGNALS:
-    void cam_switch(quint8 cam_id);
+	void cam_switch(quint8 cam_id);
 
 private slots:
 
 //    void onUARTread();
 //    void parseReceive();
-    void on_blink();
-    void onCamSwitch(quint8 camId);
-    void on_bTestUpdate_clicked();
-    void onFramesChanged(int displayFrames, int lostFrames);
-    void onMonitorError();
-    void setSystemDateTime(QDateTime dt);
-    void startLoggers();
+	void on_blink();
+	void onCamSwitch(quint8 camId);
+	void on_bTestUpdate_clicked();
+	void onFramesChanged(int displayFrames, int lostFrames);
+	void onMonitorError();
+	void setSystemDateTime(QDateTime dt);
+	void startLoggers();
 
-    void reqAppState();
-    void errorPacket(QByteArray packet, bool crc);
-    void onSwitchTimer();
+	void reqAppState();
+	void errorPacket(QByteArray packet, bool crc);
+	void onSwitchTimer();
 
 private:
-    void closeEvent(QCloseEvent* event) override;
-    void showEvent  (QShowEvent*   event) override;
+	void closeEvent(QCloseEvent* event) override;
+	void showEvent  (QShowEvent*   event) override;
 #ifdef DESKTOP_DEBUG_BUILD
-    void keyReleaseEvent(QKeyEvent* event) override;
+	void keyReleaseEvent(QKeyEvent* event) override;
 #endif
 
 
-    void initBlinker();
+	void initBlinker();
 
-    void startCamMonitor();
-    void init_gpio  ();
-    void init_libvlc();
-    void load_config();
-    void init_uart  ();
+	void startCamMonitor();
+	void init_gpio  ();
+	void init_libvlc();
+	void load_config();
+	void init_uart  ();
 
-    void deinitUART ();
-    void deinitLoggers();
-    void deinit_all ();
+	void deinitUART ();
+	void deinitLoggers();
+	void deinit_all ();
 
-    void start_file_deleter();
-    void readCPUtemper();
-    QString whoami();
-    bool check_media_drive();
+	void start_file_deleter();
+	void readCPUtemper();
+	bool fanSwitch(bool on);
+	QString whoami();
+	bool check_media_drive();
 
-    quint8 devId;
-    QString m_vlog_root  ;
-    QWidget* m_CamWidget = nullptr;
+	quint8 devId;
+	QString m_vlog_root  ;
+	QWidget* m_CamWidget = nullptr;
 
 
-    static constexpr int PLAYER_RESPONSE_TIMEOUT = 10000;
-    QTimer playerResponseTimer;
+	static constexpr int PLAYER_RESPONSE_TIMEOUT = 10000;
+	QTimer playerResponseTimer;
 
-    QList<cam_params_t> readCameraList();
-    QVector<cam_logger*>   loggers;
-    cam_logger* cam_monitor = nullptr;
-    void onStartMon();
-    void onStopMon();
+	QList<cam_params_t> readCameraList();
+	QVector<cam_logger*>   loggers;
+	cam_logger* cam_monitor = nullptr;
+	void onStartMon();
+	void onStopMon();
 
-    QTimer blinker ;
-    QTimer parser  ;
-    QTimer starLoggersTimer ;
-    QTimer switchTimer;
-    RecvParser recvParser;
-    PCK_STATE_t        appState ;
+	QTimer blinker ;
+	QTimer parser  ;
+	QTimer starLoggersTimer ;
+	QTimer switchTimer;
+	RecvParser recvParser;
+	PCK_STATE_t        appState ;
 
-    bool               led_state = true;
+	bool               led_state = true;
 
-    QSerialPort*       uart = Q_NULLPTR;
-    FileDeleterThread* file_deleter = Q_NULLPTR;
+	QSerialPort*       uart = Q_NULLPTR;
+	FileDeleterThread* file_deleter = Q_NULLPTR;
 
-    static const char* const vlcArgs[];
-    static bool    do_rename_recorder  ();
-    static QString get_update_file_name();
-    static void    check_need_update   ();
+	static const char* const vlcArgs[];
+	static bool    do_rename_recorder  ();
+	static QString get_update_file_name();
+	static void    check_need_update   ();
 
-    void deinitMonitor();
-    void deinitFileDeleter();
-    void activateSelf();
-    void initStartLoggers();
-    void initRecvParser(QIODevice* io);
+	void deinitMonitor();
+	void deinitFileDeleter();
+	void activateSelf();
+	void initStartLoggers();
+	void initRecvParser(QIODevice* io);
 
-    void initCamMonitor();
+	void initCamMonitor();
+
 };
 
 #ifndef DESKTOP_DEBUG_BUILD
-    #include <wiringPi.h>
+	#include <wiringPi.h>
 #else
-    #define digitalRead(x)
-    #define digitalWrite(x,y)
+	#define digitalRead(x)
+	#define digitalWrite(x,y)
 
 #endif
 
